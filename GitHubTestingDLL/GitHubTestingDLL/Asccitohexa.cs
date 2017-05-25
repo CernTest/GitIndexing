f0e5e7c06c83d1db405a7a3f50605f7480efd7b5
Venkatachalaiah <ananthakrishnav@gmail.com>
2017-05-25 15:48:01 +0530

    Adding GITHUBTESTING projects

---
+++ GitHubTestingDLL/GitHubTestingDLL/Asccitohexa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTestingDLL
+{
+    public class Asccitohexa
+    {
+
+        public static int String2Hex(ref string S1, ref string sname2)
+        {
+
+            string result = String.Empty;
+
+            foreach(char c in  S1.ToCharArray())
+            {
+                if (string.CompareOrdinal(c.ToString(), "\r") == 0)
+                {
+                    result = S1.Replace("\r", "[0D]\n");
+                    sname2 = result;
+                }
+            }
+
+            return 0;
+        }
+
+        public static void printstring(ref string s1)
+        {
+
+            Console.WriteLine(s1);
+        }
+    }
+}
---
+++ GitHubTestingEXE/GitHubTestingEXE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubTestingDLL;
+
+namespace GitHubTestingEXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s1 = "this is a string variable";
+            string s2 = null;            
+
+            Asccitohexa.String2Hex(ref s1, ref s2);
+
+            Asccitohexa.printstring(ref s1);
+            Asccitohexa.printstring(ref s2);
+
+        }
+    }
+}
